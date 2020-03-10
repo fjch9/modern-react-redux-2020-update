38d4cbdbc4351b17ee6b8b9635aa70c9f7da85fe
Frank Chaves <fjch.salazar@gmail.com>
2020-03-10 10:29:15 -0600

    minor update

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,22 @@
 //Import the React and ReactDOM Libraries
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 //Create a React Component
 const App = () => {
-  return <div> Hello there! </div>;
+  const buttonLabel = "Click Me!";
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text"></input>
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonLabel}
+      </button>
+    </div>
+  );
 };
 
 //Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-export default class index extends Component {
-  render() {
-    return <div></div>;
-  }
-}
